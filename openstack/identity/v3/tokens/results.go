kwapik/gophercloud

Add Roles and Project to Token

openstack/identity/v3/tokens/results.go
@@ -3,6 +3,7 @@ package tokens
 import (
 	"time"
 
+	"github.com/kwapik/gophercloud/openstack/identity/v3/projects"
 	"github.com/mitchellh/mapstructure"
 	"github.com/rackspace/gophercloud"
 )
@@ -120,12 +121,17 @@ func createErr(err error) CreateResult {
 
 // GetResult is the deferred response from a Get call.
 type GetResult struct {
-	commonResult
+	CreateResult
 }
 
 // RevokeResult is the deferred response from a Revoke call.
 type RevokeResult struct {
 	commonResult
+}
+
+type Role struct {
+	ID   string `mapstructure:"id"`
+	Name string `mapstructure:"name"`
 }
 
 // Token is a string that grants a user access to a controlled set of services in an OpenStack provider.
@@ -136,4 +142,10 @@ type Token struct {
 
 	// ExpiresAt is the timestamp at which this token will no longer be accepted.
 	ExpiresAt time.Time
+
+	// Project provides information about the project to which this token grants access.
+	Project projects.Project
+
+	// Authorization need user info which can get from token authentication's response
+	Roles []Role
 }

kwapik/gophercloud-1

openstack/identity/v3/tokens/results.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"github.com/gophercloud/gophercloud"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/projects"
+	"github.com/gophercloud/gophercloud/openstack/identity/v3/roles"
 )
 
 // Endpoint represents a single API endpoint offered by a service.
@@ -96,6 +98,10 @@ type Token struct {
 	ID string `json:"id"`
 	// ExpiresAt is the timestamp at which this token will no longer be accepted.
 	ExpiresAt time.Time `json:"expires_at"`
+	// Project provides information about the project to which this token grants access.
+	Project projects.Project `json:"project"`
+	// Authorization need user info which can get from token authentication's response.
+	Roles []roles.Role `json:"roles"`
 }
 
 func (r commonResult) ExtractInto(v interface{}) error {